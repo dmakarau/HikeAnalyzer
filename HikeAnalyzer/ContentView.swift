3a522df31a35d8bba157f91dfb30300adc992efb
dmakarau <denis.makarow@gmail.com>
2025-10-21 21:04:40 +0200

    Initial Commit

---
+++ HikeAnalyzer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HikeAnalyzer
+//
+//  Created by Denis Makarau on 21.10.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HikeAnalyzer/HikeAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeAnalyzerApp.swift
+//  HikeAnalyzer
+//
+//  Created by Denis Makarau on 21.10.25.
+//
+
+import SwiftUI
+
+@main
+struct HikeAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
